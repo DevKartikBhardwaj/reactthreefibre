05829dc55bd640a24fa752a6737ae545f769963e
Kartik Bhardwaj <developer.kartikbhardwaj@gmail.com>
2023-09-24 20:40:36 +0530

    lecture 02 learned about orbitControls,azimuthalangle,polarangle and also how to move 3d model when the mouse will move

--- src/Three.jsx
+++ src/Three.jsx
@@ -1,12 +1,26 @@
-import { PerspectiveCamera } from "@react-three/drei";
-import React from "react";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import React, { useEffect, useRef } from "react";
 import { degToRad } from "three/src/math/MathUtils";
 
 const Three = () => {
+  // this hook runs whenever a new frame is getting rendered
+  const orbitControlRef = useRef();
+  useFrame((state) => {
+    const { x, y } = state.mouse;
+    orbitControlRef.current.setAzimuthalAngle(-x * degToRad(30));
+    orbitControlRef.current.setPolarAngle((y + 0.75) * degToRad(60));
+  });
+
   return (
     <>
       {/* camera */}
       <PerspectiveCamera makeDefault position={[0, 1, 8]} />
+      <OrbitControls
+        ref={orbitControlRef}
+        minPolarAngle={degToRad(60)}
+        maxPolarAngle={degToRad(80)}
+      />
       {/* Ball */}
       <mesh position={[0, 1, 0]}>
         <sphereGeometry args={[1, 32, 32]} />
@@ -14,7 +28,7 @@ const Three = () => {
       </mesh>
       {/* floor */}
       <mesh rotation={[degToRad(270), 0, 0]}>
-        <planeGeometry args={[6, 6]} />
+        <planeGeometry args={[7, 7]} />
         <meshStandardMaterial color={"black"} />
       </mesh>
       {/* lights */}
