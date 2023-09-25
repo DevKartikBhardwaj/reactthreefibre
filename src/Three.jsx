66dadae76aef42265c5c35737906c3a6b13bfa06
Kartik Bhardwaj <developer.kartikbhardwaj@gmail.com>
2023-09-25 17:03:15 +0530

    lecture 05 learned how to use gsap for animation

--- src/Three.jsx
+++ src/Three.jsx
@@ -4,10 +4,10 @@ import {
   PerspectiveCamera,
 } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
+import gsap from "gsap";
 import React, { useEffect, useRef } from "react";
 import { BackSide } from "three";
 import { degToRad } from "three/src/math/MathUtils";
-import * as THREE from "three";
 const Three = () => {
   // this hook runs whenever a new frame is getting rendered
   // const orbitControlRef = useRef();
@@ -17,20 +17,50 @@ const Three = () => {
   //   orbitControlRef.current.setPolarAngle((y + 0.75) * degToRad(60));
   // });
 
+  //Animations
+  const ballRef = useRef(null);
+
+  useEffect(() => {
+    if (!!ballRef.current) {
+      console.log(ballRef.current);
+
+      const timeline = gsap.timeline();
+
+      // x-axis animation
+      timeline.to(ballRef.current.position, {
+        x: 2,
+        duration: 2,
+        ease: "power2.out",
+      });
+
+      //y-axis motion
+      timeline.to(
+        ballRef.current.position,
+        {
+          y: 0.5,
+          duration: 0.75,
+          ease: "bounce.out",
+        },
+        "<"
+      );
+    }
+  }, [ballRef.current]);
+
   return (
     <>
       {/* camera */}
-      <PerspectiveCamera makeDefault position={[0, 1, 8]} />
+      <PerspectiveCamera makeDefault position={[0, 1, 6]} />
       <OrbitControls
       // ref={orbitControlRef}
       // minPolarAngle={degToRad(60)}
       // maxPolarAngle={degToRad(80)}
       />
       {/* Ball */}
-      <mesh position={[0, 1, 0]} castShadow>
-        <sphereGeometry args={[1, 32, 32]} />
+      <mesh position={[0, 2.5, 0]} castShadow ref={ballRef}>
+        <sphereGeometry args={[0.5, 32, 32]} />
         <meshStandardMaterial color="#ffffff" />
       </mesh>
+
       {/* floor */}
       <mesh rotation={[degToRad(270), 0, 0]} receiveShadow>
         <planeGeometry args={[8, 8]} />
